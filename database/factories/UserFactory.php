3887005c116012600629ca10e6416e17dcaa0529
marb61a <marb61a@gmail.com>
2017-11-13 17:23:55 +0000

    added TodosController and updated UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,9 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$faker->define(App\Todo::class, function(Faker\Generator $faker){
+	return [
+
+	];
+});
