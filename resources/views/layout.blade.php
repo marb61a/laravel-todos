ab716be24b70265dfef1bb0de4e37abc610a4f1d
marb61a <marb61a@gmail.com>
2017-11-21 16:49:44 +0000

    chenged view files to use layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -70,15 +70,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    @yield('content')
                 </div>
             </div>
         </div>
