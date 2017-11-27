6887ed547f28025ac308af582ad8e076745c2ec5
marb61a <marb61a@gmail.com>
2017-11-27 17:03:07 +0000

    modified routes

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,26 @@ Route::get('/todos', [
 	'uses' => 'TodosController@index'
 ]);
 
+Route::get('todo/delete/{id}', [
+	'uses' => 'TodosController@delete',
+	'as' => 'todo.delete'	
+]);
+
+Route::get('todo/update/{id}', [
+	'uses' => 'TodosController@update',
+	'as' => 'todo.update'	
+]);
+
+Route::post('todo/save/{id}', [
+	'uses' => 'TodosController@save',
+	'as' => 'todos.save'	
+]);
+
 Route::post('/create/todo', [
 	'uses' => 'TodosController@store'
+]);
+
+Route::get('todos/completed/{id}', [
+	'uses' => 'TodosController@completed',
+	'as' => 'todo.completed'	
 ]);
